c4cd5a076d2dedd96ae69b439dd0e84cdf0d945f
Danilo Giarlini <danilogiarlini@gmail.com>
2023-09-19 19:09:12 +0200

    dropdown completed

---
+++ src/components/dropdown/CompanyDropdown.js
@@ -0,0 +1,13 @@
+import './CompanyDropdown.css'
+
+const CompanyDropdown = () => {
+    return <div>
+        <ul className="dropdown-menu">
+            <li>History</li>
+            <li>Our Team</li>
+            <li>Blog</li>
+        </ul>
+    </div>
+}
+
+export default CompanyDropdown
